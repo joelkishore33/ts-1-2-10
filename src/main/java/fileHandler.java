b66c52467d593315e2544a43c746e310859a85b2
pelin-sayar <pelinsayar14@gmail.com>
2026-02-11 02:04:08 -0500

    created fileHandler.java

--- src/main/java/fileHandler.java
+++ src/main/java/fileHandler.java
@@ -1,16 +1,73 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class fileHandler {
 
     public String[] readFile(){
-        return null;
+        //returns String aray of all files in the data directory by name
+        String path = "./data";
+        File directory = new File(path);
+
+        String[] files = directory.list();
+
+        return files;
     }
 
-    public String readFile(String file){
-        return null;
+    public String readFile(String fileName){
+        //returns the contents of the specified file using regular decrypting key
+        String path = "./data/" + fileName;
+        String contents = "";
+
+        try{
+            File file = new File(path);
+            Scanner readIn = new Scanner(file);
+
+            while(readIn.hasNextLine()){
+                contents += readIn.nextLine();
+            }
+
+            readIn.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No such file found");
+            System.exit(1);
+        }
+
+        //deciphers the contents of the specified file using regular decrypting key
+        Cipher cipher = new Cipher();
+        contents = cipher.decrypt(contents);
+
+        return contents;
     }
 
-    public String readFile(String file, String key){
-        return null;
+    public String readFile(String fileName, String key){
+        //returns the contents of the specified files using unique decrypting key
+        String path = "./data/" + fileName;
+        String contents = "";
+
+        try{
+            File file = new File(path);
+            Scanner readIn = new Scanner(file);
+
+            while(readIn.hasNextLine()){
+                contents += readIn.nextLine();
+            }
+
+            readIn.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No such file found");
+            System.exit(1);
+        }
+
+        //deciphers the contents of the specified file using unique decrypting key
+        Cipher cipher = new Cipher(key);
+        contents = cipher.decrypt(contents);
+
+        //Chipher cipher = new Cipher();
+        //contents = cipher.decrypt(contents, key);
+
+        return contents;
     }
 }
