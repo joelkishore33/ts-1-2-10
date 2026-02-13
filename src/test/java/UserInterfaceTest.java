0baaf7571ae15da1475320a3dfa2f627b0904a04
DennisNi08 <cgh7bv@virginia.edu>
2026-02-13 00:31:42 -0500

    added more UI test

--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -6,13 +6,13 @@ class UserInterfaceTest {
     private static UserInterface testInterface;
     @BeforeEach
     public void initializeUI(){
-        String[] userInput = new String[]{"filename01","abc"};
+        String[] userInput = new String[]{"filename01","abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
         testInterface = new UserInterface(userInput);
     }
 
     @Test
     void testPartitionUserInput() {
-        assertTrue(testInterface.getFileSelected().equals("filename01"));
+        assertTrue(testInterface.getFileSelected().equals("carnivore.txt"));
         assertTrue(testInterface.getAltKey().equals("abc"));
     }
 
@@ -28,10 +28,19 @@ class UserInterfaceTest {
 
     @Test
     void transferFileSelected() {
-
+        String content = "Carnivore, later renamed DCS1000, was a system implemented by the Federal Bureau of Investigation (FBI) that was\n" +
+                "designed to monitor email and electronic communications. It used a customizable packet sniffer that could monitor all\n" +
+                "of a target user's Internet traffic. Carnivore was implemented in October 1997. By 2005 it had been replaced with\n" +
+                "improved commercial software.";
+        assertEquals(content, testInterface.transferFileSelected());
     }
 
     @Test
     void transferFileSelectedWithAltKey() {
+        String content = "Dbsojwpsf, mbufs sfobnfe EDT2aaa, xbt b tztufn jnqmfnfoufe cz uif Gfefsbm Cvsfbv pg Jowftujhbujpo (GCJ) uibu xbt\n" +
+                "eftjhofe up npojups fnbjm boe fmfduspojd dpnnvojdbujpot. Ju vtfe b dvtupnjAbcmf qbdlfu tojggfs uibu dpvme npojups bmm\n" +
+                "pg b ubshfu vtfs't Joufsofu usbggjd. Dbsojwpsf xbt jnqmfnfoufe jo Pdupcfs 2008. Cz 3aa6 ju ibe cffo sfqmbdfe xjui\n" +
+                "jnqspwfe dpnnfsdjbm tpguxbsf.";
+        assertEquals(content, testInterface.transferFileSelectedWithAltKey());
     }
 }
