c27a6a479ae6e77169b249d39f57463e45435faf
pelin-sayar <pelinsayar14@gmail.com>
2026-02-13 01:00:01 -0500

    changes to main method

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,19 +1,22 @@
-import java.io.IOException;
-
 /**
  * Commmand Line Utility
  */
 public class TopSecret {
     public static void main(String[]args){
-        if (args.length==0){
-            throw new IllegalArgumentException("Not enough arguments provided");
+        try{
+            UserInterface UI = new UserInterface(args);
+            if(args.length ==1){
+                System.out.println(UI.transferFileSelected());
+            }
+            else if(args.length == 2){
+                System.out.println(UI.transferFileSelectedWithAltKey());
+            }
+            else{
+                System.out.println("Invalid number of arguments.");
+            }
         }
-        UserInterface UI = new UserInterface(args);
-        if(args.length ==1){
-            System.out.println(UI.transferFileSelected());
-        }
-        else if(args.length == 2){
-            System.out.println(UI.transferFileSelectedWithAltKey());
+        catch(Exception e){
+            System.out.println("An error occured." + e.getMessage());
         }
     }
 }
